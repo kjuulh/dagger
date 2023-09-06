942da71fe6ae593934b5ed628b13e31148f0f56c
kjuulh <contact@kjuulh.io>
2023-09-06 23:24:51 +0200

    refactor: change exposed main type
    This changes the main exposed type from dagger_sdk::connect
    
    from Arc<Query> to an opaque type: `Dagger`.
    
    Which defers to Arc<Query> in the background. But makes it much easier to use in apis
    
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- sdk/rust/crates/dagger-sdk/src/client.rs
+++ sdk/rust/crates/dagger-sdk/src/client.rs
@@ -1,3 +1,4 @@
+use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 
 use crate::core::config::Config;
@@ -9,15 +10,36 @@ use crate::gen::Query;
 use crate::logging::StdLogger;
 use crate::querybuilder::query;
 
-pub type DaggerConn = Arc<Query>;
+#[derive(Clone)]
+pub struct Dagger(Arc<Query>);
 
-pub async fn connect() -> Result<DaggerConn, ConnectError> {
+impl Deref for Dagger {
+    type Target = Arc<Query>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Dagger {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl From<Arc<Query>> for Dagger {
+    fn from(value: Arc<Query>) -> Self {
+        Self(value)
+    }
+}
+
+pub async fn connect() -> Result<Dagger, ConnectError> {
     let cfg = Config::new(None, None, None, None, Some(Arc::new(StdLogger::default())));
 
     connect_opts(cfg).await
 }
 
-pub async fn connect_opts(cfg: Config) -> Result<DaggerConn, ConnectError> {
+pub async fn connect_opts(cfg: Config) -> Result<Dagger, ConnectError> {
     let (conn, proc) = DaggerEngine::new()
         .start(&cfg)
         .await
@@ -27,7 +49,8 @@ pub async fn connect_opts(cfg: Config) -> Result<DaggerConn, ConnectError> {
         proc: proc.map(|p| Arc::new(p)),
         selection: query(),
         graphql_client: Arc::new(DefaultGraphQLClient::new(&conn)),
-    }))
+    })
+    .into())
 }
 
 // Conn will automatically close on drop of proc
